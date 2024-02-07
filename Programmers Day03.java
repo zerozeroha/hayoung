2a6d5e96882f7ff1652afef1772cb803cb55d6fd
ha young <158129874+zerozeroha@users.noreply.github.com>
2024-02-07 11:15:29 +0900

    Update Programmers Day03.java

--- Programmers Day03.java
+++ Programmers Day03.java
@@ -1,18 +1,31 @@
 //1. 문자열 섞기
+
+/*< String substring (int start.int end)>
+: 지정된 범위 내의 문자열을 String으로 반환.
+시작위치(start)지점만 지정하면 끝까지 반환. 끝자리 : (int end-1)*/
+//예제
+StringBuffer sb = new StringBuffer ("0123456")
+    String str = sb. substring(3); // 결과 :  str = "3456"
+    String str = sb. substring(3,5); // 결과 : str = "34"
+
+
+/*길이가 같은 두 문자열 str1과 str2가 주어집니다.
+
+두 문자열의 각 문자가 앞에서부터 서로 번갈아가면서 한 번씩 등장하는 문자열을 만들어 return 하는 solution 함수를 완성해 주세요.*/
+//SOLUTION
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         
         for(int i=0; i<str1.length(); i++) {
             answer += str1.substring(i,i+1);
-            answer += str2.substring(i,i+1);
-            
-        }
-        
-        
+            answer += str2.substring(i,i+1);     
+        } 
         return answer;
     }
 }
+
+
 
 //2. 문자리스트를 문자열로 변환하기
 class Solution {
