41e28d6d23f96d615bc40056c45d26fe1d99564d
zerozeroha <158129874+zerozeroha@users.noreply.github.com>
2024-03-06 19:58:04 +0900

    Update 24.03.06  과제 (주찬).java

--- 24.03.06  과제 (주찬).java
+++ 24.03.06  과제 (주찬).java
@@ -1,3 +1,27 @@
+
+< 문제 >
+  
+// 학생 목록에서 성적이 80점 이상인 학생들의 목록을 필터링하는 function을 작성해 콘솔로 출력
+
+function filterHighScorers(students) {
+ 
+}
+
+//학생 , 점수 목록 :  홍길동 74점 , 손흥민 98점, 이강인 81점 , 황희찬 65점 
+
+const students = [
+
+];
+
+//콘솔출력
+
+
+
+
+
+
+< 풀이 >
+
 // 주어진 학생 목록에서 성적이 80점 이상인 학생들의 목록을 필터링하는 함수
 function filterHighScorers(students) {
 // filter 메서드를 사용하여 성적이 80점 이상인 학생들만 필터링하여 반환
